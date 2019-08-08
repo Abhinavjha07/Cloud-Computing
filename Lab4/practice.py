8711318f99892363391890456ec37b8487091dbe
Abhinav Jha <31755654+Abhinavjha07@users.noreply.github.com>
2019-08-08 10:09:01 +0530

    Delete practice.py

--- Lab4/practice.py
+++
@@ -1,4 +0,0 @@
-import boto.ec2
-access_key_id='AKIAJFXCKF4HG2Q2MMZA';
-secret_access_key='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';
-con=boto.ec2.connect_to_region('us-west-2',aws_access_key_id=access_key_id,aws_secret_access_key=secret_access_key)
